fe4650ddfcad389c4e1fda267cccce3f0aa59b21
JeremyCurmi <jeremycurmi13@gmail.com>
2022-05-08 00:06:57 +0200

    mvp set up: - Dockerfile running python3.10 - working sample with cv2 and pytesseract

---
+++ main.py
@@ -0,0 +1,16 @@
+import cv2
+import pytesseract
+
+
+def main():
+    image_path = "tests/artifacts/image.jpg"
+    img = cv2.imread(image_path)
+
+    # Adding custom options
+    custom_config = r'--oem 3 --psm 6'
+    output = pytesseract.image_to_string(img, config=custom_config)
+    print("output: \n {}".format(output))
+
+
+if __name__ == '__main__':
+    main()
